eab5bd894f433ea942dba74e486e17f395554eb1
范淼 <femiajayi705@gmail.com>
2021-02-07 23:43:53 +0800

    Testing pushing production image to docker hub

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # print outputs ond exit on first filure
-set -xe
-if [$TRAVIS_BRANC == 'master']; then
+##set -xe
+##if [$TRAVIS_BRANC == 'master']; then
 
     # setup ssh agent, git config and remote
-    eval "$(ssh-ageng -s)"
-    ssh-add ~/.ssh/test_travis_rsa
+##    eval "$(ssh-ageng -s)"
+##    ssh-add ~/.ssh/test_travis_rsa
     # Dont know what to do with git here
 
     # Pull the latest docker images
-    docker build .
+##    docker build .
 
     # Stop exisiting containers 
 
     # Start a new container using the latest image
 
-fi
+##fi
