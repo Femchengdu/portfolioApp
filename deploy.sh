57361c11a9ddab3582156e94537e6524524cc44a
范淼 <femiajayi705@gmail.com>
2021-02-08 14:34:36 +0800

    Returned ip numbers to the deploy sh file

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,13 @@
-# print outputs ond exit on first filure
+
 set -xe
 
     echo "Running deploy script"
-    # setup ssh agent, git config and remote
+
     eval "$(ssh-agent -s)"
     echo "Agent started"
     ssh-add ~/.ssh/id_rsa
     echo "Added keys"
     echo "Trying to ssh into digital ocean:"
-    ssh test_travis@$DEPLOY_IP < ./success.sh
+    ssh test_travis@157.245.252.42 < ./success.sh
 
-    # Dont know what to do with git here
-
-    # Pull the latest docker images
-##    docker build .
-
-    # Stop exisiting containers 
-
-    # Start a new container using the latest image
 
