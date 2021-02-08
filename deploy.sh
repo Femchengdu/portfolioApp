c62aae0b6f66b555657d5bcfde92af0a961fa20a
范淼 <femiajayi705@gmail.com>
2021-02-08 14:50:17 +0800

    Failing foreward

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ set -xe
     ssh-add ~/.ssh/id_rsa
     echo "Added keys"
     echo "Trying to ssh into digital ocean:"
-    ssh -t -t -R test_travis@157.245.252.42 < ./success.sh
+    ssh -t -t -R test_travis@157.245.252.42 "docker -v >> success.txt" "exit"
+    echo "Succesful writ ? written version "
+    echo "Maybe success ? writing docker version "
 
 
--- success.sh
+++
@@ -1,5 +0,0 @@
-set -xe
-    docker -v >> success.txt
-    exit
-    echo "Succesful writ ? written version "
-    echo "Maybe success ? writing docker version "
