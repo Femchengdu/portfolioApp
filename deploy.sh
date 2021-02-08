0587205a6185144968920aba7b7afd382b156ef9
范淼 <femiajayi705@gmail.com>
2021-02-08 13:07:40 +0800

    Testing a basic deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+!/bin/bash
 
 # print outputs ond exit on first filure
-##set -xe
-##if [$TRAVIS_BRANC == 'master']; then
-
+set -xe
+if [$TRAVIS_BRANC == 'master']; then
+    echo "Running deploy script"
     # setup ssh agent, git config and remote
-##    eval "$(ssh-ageng -s)"
-##    ssh-add ~/.ssh/test_travis_rsa
+    eval "$(ssh-agent -s)"
+    echo "Agent started"
+    ssh-add ~/.ssh/id_rsa
+    echo "Added keys"
+    echo "Trying to ssh into digital ocean:"
+    ssh test_travis@157.245.252.42
+    echo "Maybe success ? writing docker version "
+    docker -v >> success.txt
+        echo "Succesful writ ? written version "
     # Dont know what to do with git here
 
     # Pull the latest docker images
@@ -16,4 +23,4 @@
 
     # Start a new container using the latest image
 
-##fi
+fi
