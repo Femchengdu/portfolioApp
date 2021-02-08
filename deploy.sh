8c89bf861583594c9f6cd66bbc01841925baf0c6
范淼 <femiajayi705@gmail.com>
2021-02-08 14:55:08 +0800

    Removed command line flags

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ set -xe
     ssh-add ~/.ssh/id_rsa
     echo "Added keys"
     echo "Trying to ssh into digital ocean:"
-    ssh -t -t -R test_travis@157.245.252.42 "docker -v >> success.txt" "exit"
+    ssh test_travis@157.245.252.42 "docker -v >> success.txt exit"
     echo "Succesful writ ? written version "
     echo "Maybe success ? writing docker version "
 
