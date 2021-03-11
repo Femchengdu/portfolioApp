b96031faa5b50e8aab30cc0617fac00dd65898c3
范淼 <femiajayi705@gmail.com>
2021-03-11 13:52:04 +0800

    refactor: Passed the success.sh script to deploy.sh

--- success.sh
+++ success.sh
@@ -1,5 +1,14 @@
 set -xe
-eval "$(docker -v >> not_a_fluke.txt)"
+echo "Changing into portfolio directory"
+cd portfolio
+echo "Tearing down site"
+sudo docker-compose down
+echo "Stopped all containers"
+sudo docker ps
+echo "Removing dangling containers"
+yes | sudo docker system prune
+echo "Removing previous images"
+sudo docker rmi rdevcd2020/express-react-feb8
 exit
 echo "If you get here, you need to receive your masters"
 echo "Deployment successful!!!!"
