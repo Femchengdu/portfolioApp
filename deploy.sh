a9c7b9cad171d08166755351527fb4ea20eab00a
范淼 <femiajayi705@gmail.com>
2021-03-11 21:09:17 +0800

    refactor: Modified  deploy.sh file to make exit the last command

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,8 @@ ssh-add ~/.ssh/id_rsa
 echo "Added keys"
 echo "Trying to ssh into digital ocean:"
 ssh test_travis@157.245.252.42 < success.sh
-
 echo "After success.sh script" 
-exit
-echo "If you get here, you need to receive your masters"
 echo "Deployment successful!!!!"
+exit
 
 
