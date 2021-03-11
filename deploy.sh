ca687548943e5d743b8af90575f09bbf33fba5f4
范淼 <femiajayi705@gmail.com>
2021-03-11 13:29:29 +0800

    feat: deploy experiment script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,16 @@ ssh-add ~/.ssh/id_rsa
 echo "Added keys"
 echo "Trying to ssh into digital ocean:"
 ssh test_travis@157.245.252.42 'load your image here exit'
-
+echo "Tearing down site"
+sudo docker-compose down
+echo "Stopped all containers"
+sudo docker ps
+echo "Removing dangling containers"
+yes | sudo docker system prune
+echo "Removing previous images"
+sudo docker rmi rdevcd2020/express-react-feb8
+exit
+echo "If you get here, you need to receive your masters"
 echo "Deployment successful!!!!"
     
 
