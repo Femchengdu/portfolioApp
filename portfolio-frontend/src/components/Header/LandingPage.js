dd461f11c274042e8d982dfe472444df3b8dca4b
范淼 <femiajayi705@gmail.com>
2021-03-12 02:18:15 +0800

    styling: Added conditional styling to the header component

--- portfolio-frontend/src/components/Header/LandingPage.js
+++ portfolio-frontend/src/components/Header/LandingPage.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import HeaderLandingContent from "../Header/HeaderLandingContent";
 import CenterComponent from "../Containers/CenterComponent";
 import MainNavContainer from "../Nav/MainNavContainer";
 import MainNavLogo from "../Nav/MainNavLogo";
 import MainNavList from "../Nav/MainNavList";
+import AppContext from "../../context/AppContext";
 import "./landing.css";
 const LandingPage = () => {
+  const { appPage } = useContext(AppContext);
   return (
-    <header id="header-home">
-      <div className="backgroundImg">
-        <CenterComponent addTintPosition={true}>
-          <MainNavContainer>
-            <MainNavLogo />
-            <MainNavList />
-          </MainNavContainer>
-          <HeaderLandingContent />
-        </CenterComponent>
-      </div>
+    <header
+      id="header-home"
+      className={appPage === "index" ? "backgroundImg" : "backgroundImgNoIndex"}
+    >
+      <CenterComponent addTintPosition={true}>
+        <MainNavContainer>
+          <MainNavLogo />
+          <MainNavList />
+        </MainNavContainer>
+
+        {appPage === "index" ? <HeaderLandingContent /> : null}
+      </CenterComponent>
     </header>
   );
 };
